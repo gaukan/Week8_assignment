c2c1181583cfa40a7e99cb41a3e9cbc1c9143416
gaukan <130718912+gaukan@users.noreply.github.com>
2023-04-19 09:41:57 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"22ds1000155@ds.study.iitm.ac.in\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
